a3c9bd04e97e1b430374f862c6755ef8726e68ad
Patrick Emond <emondpd@mcmaster.ca>
2024-03-12 16:20:00 -0400

    Feature: sorting pine-based home interview files

--- lib/data_type/audio.class.php
+++ lib/data_type/audio.class.php
@@ -49,34 +49,31 @@ class audio extends base
 
     // now go through files that are labelled by UID (valid audio recordings)
     $file_count = 0;
-    foreach( glob( sprintf( '%s/*/audio/*/*/*/*.wav', $base_dir ) ) as $filename )
+
+    $filename_list = array_merge(
+      // pine-based comprehensive recordings
+      glob( sprintf( '%s/nosite/*/*/*/audio.wav', $base_dir ) ),
+      // CATI tracking and onyx-based comprehensive recordings
+      glob( sprintf( '%s/nosite/audio/*/*/*/*.wav', $base_dir ) )
+    );
+
+    foreach( $filename_list as $filename )
     {
-      $matches = [];
-      if( false === preg_match( '#/([^/]+)/([^/]+)/([^/]+)/([^/]+).wav$#', $filename, $matches ) )
-      {
-        $reason = sprintf(
-          'Ignoring invalid audio file: "%s"',
-          $filename
-        );
-        self::move_from_temporary_to_invalid( $filename, $reason );
-        continue;
-      }
-
-      $cohort = $matches[1];
-      $phase_name = $matches[2];
-      $uid = $matches[3];
-      $name = $matches[4];
-
+      $uid = NULL;
       $phase = NULL;
       $study = NULL;
       $destination_filename = NULL;
-      if( 'comprehensive' == $cohort )
-      {
-        // comprehensive phase names are f1, f2, etc...
-        $study = 'clsa';
-        $phase = intval( substr( $phase_name, 1, 1 ) ) + 1;
-        $variable = preg_replace( '/_(COF[1-9]|DCS)$/', '', $name );
 
+      $matches = [];
+      // pine-based comprehensive recordings
+      if( preg_match( '#nosite/Follow-up ([0-9]) (Home|Site)/([^/]+)/([^/]+)/audio.wav$#', $filename, $matches ) )
+      {
+        $phase = $matches[1] + 1;
+        $variable = $matches[3];
+        $uid = $matches[4];
+        $study = 'clsa';
+
+        $destination_filename = NULL;
         if( 'FAS_FREC' == $variable ) $destination_filename = 'f_word_fluency';
         else if( 'FAS_AREC' == $variable ) $destination_filename = 'a_word_fluency';
         else if( 'FAS_SREC' == $variable ) $destination_filename = 's_word_fluency';
@@ -84,69 +81,111 @@ class audio extends base
         else if( 'STP_WORREC' == $variable ) $destination_filename = 'stroop_word';
         else if( 'STP_COLREC' == $variable ) $destination_filename = 'stroop_colour';
         else if( 'COG_ALPTME_REC2' == $variable ) $destination_filename = 'alphabet';
-        else if( 'COG_ALTTME_REC' == $variable ) $destination_filename = 'mental_alternation';
+        else if( in_array( $variable, ['COG_ALTTME_REC', 'COG_ALTTME_REC2'] ) )
+          $destination_filename = 'mental_alternation';
         else if( 'COG_ANMLLLIST_REC' == $variable ) $destination_filename = 'animal_fluency';
         else if( 'COG_CNTTMEREC' == $variable ) $destination_filename = 'counting';
         else if( 'COG_WRDLST2_REC' == $variable ) $destination_filename = 'delayed_word_list';
         else if( 'COG_WRDLSTREC' == $variable ) $destination_filename = 'immediate_word_list';
       }
-      else if( 'tracking' == $cohort )
+      // CATI tracking and onyx-based comprehensive recordings
+      else if( preg_match( '#nosite/audio/([^/]+)/([^/]+)/([^/]+)/([^/]+).wav$#', $filename, $matches ) )
       {
-        // tracking phase names refer to the application, study phase and instance:
-        // sabretooth_f1-live, sabretooth_f2-live, sabretooth_c1-live, etc
-        if( 0 === preg_match( '/sabretooth_(bl|cb|[cf][1-9])-live/', $phase_name, $matches ) )
-        {
-          $reason = sprintf(
-            'Ignoring invalid audio file: "%s"',
-            $filename
-          );
-          self::move_from_temporary_to_invalid( $filename, $reason );
-          continue;
-        }
+        $cohort = $matches[1];
+        $phase_name = $matches[2];
+        $uid = $matches[3];
+        $name = $matches[4];
 
-        $code = $matches[1];
-
-        if( 'bl' == $code || 'f' == substr( $code, 0, 1 ) )
+        $phase = NULL;
+        $study = NULL;
+        $destination_filename = NULL;
+        if( 'comprehensive' == $cohort )
         {
-          // bl is baseline (phase 1), otherwise f1, f2, etc...
-          $phase = 'bl' == $code ? 1 : intval( substr( $code, 1, 1 ) ) + 1;
+          // comprehensive phase names are f1, f2, etc...
           $study = 'clsa';
-        }
-        else if( 'cb' == $code || 'c' == substr( $code, 0, 1 ) )
-        {
-          // cb is baseline (phase 1), otherwise c1, c2, etc...
-          $phase = 'cb' == $code ? 1 : intval( substr( $code, 1, 1 ) ) + 1;
-          $study = 'covid_brain';
-        }
-        else
-        {
-          $reason = sprintf(
-            'Ignoring invalid audio file "%s"',
-            $filename
-          );
-          self::move_from_temporary_to_invalid( $filename, $reason );
-          continue;
-        }
+          $phase = intval( substr( $phase_name, 1, 1 ) ) + 1;
+          $variable = preg_replace( '/_(COF[1-9]|DCS)$/', '', $name );
 
-        if( !is_null( $phase ) && !is_null( $study ) )
+          if( 'FAS_FREC' == $variable ) $destination_filename = 'f_word_fluency';
+          else if( 'FAS_AREC' == $variable ) $destination_filename = 'a_word_fluency';
+          else if( 'FAS_SREC' == $variable ) $destination_filename = 's_word_fluency';
+          else if( 'STP_DOTREC' == $variable ) $destination_filename = 'stroop_dot';
+          else if( 'STP_WORREC' == $variable ) $destination_filename = 'stroop_word';
+          else if( 'STP_COLREC' == $variable ) $destination_filename = 'stroop_colour';
+          else if( 'COG_ALPTME_REC2' == $variable ) $destination_filename = 'alphabet';
+          else if( 'COG_ALTTME_REC' == $variable ) $destination_filename = 'mental_alternation';
+          else if( 'COG_ANMLLLIST_REC' == $variable ) $destination_filename = 'animal_fluency';
+          else if( 'COG_CNTTMEREC' == $variable ) $destination_filename = 'counting';
+          else if( 'COG_WRDLST2_REC' == $variable ) $destination_filename = 'delayed_word_list';
+          else if( 'COG_WRDLSTREC' == $variable ) $destination_filename = 'immediate_word_list';
+        }
+        else if( 'tracking' == $cohort )
         {
-          if( preg_match( '#([^-]+)-(in|out)#', $name, $matches ) )
+          // tracking phase names refer to the application, study phase and instance:
+          // sabretooth_f1-live, sabretooth_f2-live, sabretooth_c1-live, etc
+          if( 0 === preg_match( '/sabretooth_(bl|cb|[cf][1-9])-live/', $phase_name, $matches ) )
           {
-            $variable = strtolower( $matches[1] );
-            $operator = 'in' == $matches[2];
-            if( preg_match( '#^[0-9][0-9]$#', $variable ) ) $destination_filename = $variable;
-            else if( 'alphabet' == $variable ) $destination_filename = 'alphabet';
-            else if( 'mat alternation' == $variable ) $destination_filename = 'mental_alternation';
-            else if( 'animal list' == $variable ) $destination_filename = 'animal_fluency';
-            else if( 'counting to 20' == $variable ) $destination_filename = 'counting';
-            else if( 'rey i' == $variable ) $destination_filename = 'immediate_word_list';
-            else if( 'rey ii' == $variable ) $destination_filename = 'delayed_word_list';
-            else fatal_error( sprintf( 'Invalid filename "%s"', $filename ), 31 );
+            $reason = sprintf(
+              'Ignoring invalid audio file: "%s"',
+              $filename
+            );
+            self::move_from_temporary_to_invalid( $filename, $reason );
+            continue;
+          }
 
-            // add the operator tag if the audio is of the operator (in)
-            if( !is_null( $destination_filename ) && $operator ) $destination_filename .= '-operator';
+          $code = $matches[1];
+
+          if( 'bl' == $code || 'f' == substr( $code, 0, 1 ) )
+          {
+            // bl is baseline (phase 1), otherwise f1, f2, etc...
+            $phase = 'bl' == $code ? 1 : intval( substr( $code, 1, 1 ) ) + 1;
+            $study = 'clsa';
+          }
+          else if( 'cb' == $code || 'c' == substr( $code, 0, 1 ) )
+          {
+            // cb is baseline (phase 1), otherwise c1, c2, etc...
+            $phase = 'cb' == $code ? 1 : intval( substr( $code, 1, 1 ) ) + 1;
+            $study = 'covid_brain';
+          }
+          else
+          {
+            $reason = sprintf(
+              'Ignoring invalid audio file "%s"',
+              $filename
+            );
+            self::move_from_temporary_to_invalid( $filename, $reason );
+            continue;
+          }
+
+          if( !is_null( $phase ) && !is_null( $study ) )
+          {
+            if( preg_match( '#([^-]+)-(in|out)#', $name, $matches ) )
+            {
+              $variable = strtolower( $matches[1] );
+              $operator = 'in' == $matches[2];
+              if( preg_match( '#^[0-9][0-9]$#', $variable ) ) $destination_filename = $variable;
+              else if( 'alphabet' == $variable ) $destination_filename = 'alphabet';
+              else if( 'mat alternation' == $variable ) $destination_filename = 'mental_alternation';
+              else if( 'animal list' == $variable ) $destination_filename = 'animal_fluency';
+              else if( 'counting to 20' == $variable ) $destination_filename = 'counting';
+              else if( 'rey i' == $variable ) $destination_filename = 'immediate_word_list';
+              else if( 'rey ii' == $variable ) $destination_filename = 'delayed_word_list';
+              else fatal_error( sprintf( 'Invalid filename "%s"', $filename ), 31 );
+
+              // add the operator tag if the audio is of the operator (in)
+              if( !is_null( $destination_filename ) && $operator ) $destination_filename .= '-operator';
+            }
           }
         }
+      }
+      else
+      {
+        $reason = sprintf(
+          'Ignoring invalid audio file: "%s"',
+          $filename
+        );
+        self::move_from_temporary_to_invalid( $filename, $reason );
+        continue;
       }
 
       if( !is_null( $study ) && !is_null( $phase ) && !is_null( $destination_filename ) )
@@ -161,13 +200,17 @@ class audio extends base
         );
 
         // make sure the directory exists (recursively)
-        if( !is_dir( $destination_directory ) ) mkdir( $destination_directory, 0755, true );
+        if( !is_dir( $destination_directory ) )
+        {
+          if( VERBOSE ) output( sprintf( 'mkdir -m 0755 %s', $destination_directory ) );
+          if( !TEST_ONLY ) mkdir( $destination_directory, 0755, true );
+        }
 
         // determine the destination filename based on
         $destination = sprintf( '%s/%s.wav', $destination_directory, $destination_filename );
         if( TEST_ONLY ? true : copy( $filename, $destination ) )
         {
-          if( VERBOSE ) output( sprintf( '"%s" => "%s"', $filename, $destination ) );
+          if( VERBOSE ) output( sprintf( 'cp "%s" "%s"', $filename, $destination ) );
           if( !TEST_ONLY && !KEEP_FILES ) unlink( $filename );
           $file_count++;
         }
